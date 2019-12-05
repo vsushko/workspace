120d55c22f0c4578eae72bf1a7fe8238e9ddd666
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-12-05 17:59:37 +0300

    Update spark.scala

--- spark.scala
+++ spark.scala
@@ -209,3 +209,93 @@ rdd.mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
 val input = sc.textFile("s3://...")
 val words = input.flatMap(x => x.split(" "))
 val result = words.map(x => (x, 1)).reduceByKey((x, y) => x + y)
+
+                  // Per-key average using combineByKey() in Scala
+val result = input.combineByKey(
+(v) => (v, 1),
+(acc: (Int, Int), v) => (acc._1 + v, acc._2 + 1),
+(acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+).map{ case (key, value) => (key, value._1 / value._2.toFloat) }
+result.collectAsMap().map(println(_))
+
+reduceByKey() with custom parallelism in Scala
+val data = Seq(("a", 3), ("b", 4), ("a", 1))
+sc.parallelize(data).reduceByKey((x, y) => x + y) // Default parallelism
+sc.parallelize(data).reduceByKey((x, y) => x + y) // Custom parallelism
+
+// check the size of the RDD
+rdd.partitions.size()
+
+// group data sharing the same key from multiple RDDs
+x = sc.parallelize([("a", 1), ("b", 4)])
+y = sc.parallelize([("a", 2)])
+z = x.cogroup(y).collect()
+
+// Scala shell inner join
+storeAddress = {
+    (Store("Ritual"), "1026 Valencia St"), (Store("Philz"), "748 Van Ness Ave"),
+    (Store("Philz"), "3101 24th St"), (Store("Starbucks"), "Seattle")}
+
+storeRating = {
+(Store("Ritual"), 4.9), (Store("Philz"), 4.8))}
+
+// Join
+storeAddress.join(storeRating) == {
+(Store("Ritual"), ("1026 Valencia St", 4.9)),
+(Store("Philz"), ("748 Van Ness Ave", 4.8)),
+(Store("Philz"), ("3101 24th St", 4.8))}
+
+// leftOuterJoin() and rightOuterJoin()
+storeAddress.leftOuterJoin(storeRating) ==
+{(Store("Ritual"),("1026 Valencia St",Some(4.9))),
+(Store("Starbucks"),("Seattle",None)),
+(Store("Philz"),("748 Van Ness Ave",Some(4.8))),
+(Store("Philz"),("3101 24th St",Some(4.8)))}
+storeAddress.rightOuterJoin(storeRating) ==
+{(Store("Ritual"),(Some("1026 Valencia St"),4.9)),
+(Store("Philz"),(Some("748 Van Ness Ave"),4.8)),
+(Store("Philz"), (Some("3101 24th St"),4.8))}
+
+// Custom sort order in Scala, sorting integers as if strings
+val input: RDD[(Int, Venue)] = ...
+implicit val sortIntegersByString = new Ordering[Int] {
+    override def compare(a: Int, b: Int) = a.toString.compare(b.toString)
+}
+rdd.sortByKey()
+
+// Actions Available on Pair RDDs
+
+// Count the number of elements for each key. 
+rdd.countByKey()
+
+// Collect the result as a map to provide easy lookup.
+rdd.collectAsMap()
+
+// Return all values associated with the provided key.
+rdd.lookup(3)
+
+// Data Partitioning (Advanced)
+// Initialization code; we load the user info from a Hadoop SequenceFile on HDFS.
+// This distributes elements of userData by the HDFS block where they are found,
+// and doesn't provide Spark with any way of knowing in which partition a
+// particular UserID is located.
+val sc = new SparkContext(...)
+val userData = sc.sequenceFile[UserID, UserInfo]("hdfs://...").persist()
+// Function called periodically to process a logfile of events in the past 5 minutes;
+
+// we assume that this is a SequenceFile containing (UserID, LinkInfo) pairs.
+def processNewLogs(logFileName: String) {
+    val events = sc.sequenceFile[UserID, LinkInfo](logFileName)
+    val joined = userData.join(events)// RDD of (UserID, (UserInfo, LinkInfo)) pairs
+    val offTopicVisits = joined.filter {
+        case (userId, (userInfo, linkInfo)) => // Expand the tuple into its components
+            !userInfo.topics.contains(linkInfo.topic)
+    }.count()
+    println("Number of visits to non-subscribed topics: " + offTopicVisits)
+}
+
+// Scala custom partitioner
+val sc = new SparkContext(...)
+val userData = sc.sequenceFile[UserID, UserInfo]("hdfs://...")
+        .partitionBy(new HashPartitioner(100)) // Create 100 partitions
+        .persist()
